16c713169221a35cd7c32a7f68ca70d03a8e9ce0
Mart Ammon <mart@liovic.eu>
2017-10-03 15:25:10 +0300

    Tehtud midagi

--- app/src/main/java/eu/marxt12372/drive/MapUpdaterThread.java
+++ app/src/main/java/eu/marxt12372/drive/MapUpdaterThread.java
@@ -1,6 +1,8 @@
 package eu.marxt12372.drive;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapUpdaterThread extends Thread
 {
@@ -13,6 +15,22 @@ public class MapUpdaterThread extends Thread
 
     public void run()
     {
-        //Update the map
+		while(true)
+		{
+			MarkerOptions marker = new MarkerOptions();
+			marker.position(new LatLng(0,0));
+			marker.title("abcd");
+			_map.addMarker(marker);
+			//TODO: Noh, tee midagi siin siis?
+			//TODO: http://marxt12372.eu/tthk-android-drive/api/
+			try
+			{
+				Thread.sleep(5000);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
     }
 }
