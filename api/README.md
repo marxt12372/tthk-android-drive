4e47e711afd1197432062c80d6283802547e982f
Mart Ammon <mart@liovic.eu>
2017-11-08 14:53:04 +0200

    Tehtud natuke backend osa

---
+++ api/README.md
@@ -0,0 +1,6 @@
+SÃidu Staatused:
+<ul>
+<li>0 - SÃit tehtud, sÃitjat pole veel mÃÃratud.</li>
+<li>1 - SÃitja mÃÃratud, pole veel vastu vÃtnud</li>
+<li>2 - SÃidutaja on sÃidu vastu vÃtnud ja tema poole liikuma hakanud</li>
+</ul>
--- api/cronUpdater.php
+++ api/cronUpdater.php
@@ -7,7 +7,45 @@ require 'inc/config.php';
 
 $mysqli = new mysqli($mysql['host'], $mysql['user'], $mysql['pass'], $mysql['base']);
 
-//TODO: Tee midagi eluga.
+$query = $mysqli->query("SELECT * FROM " . $mysql['pref'] . "soidud");
+while($row = $query->fetch_assoc())
+{
+	if($row['staatus'] == 0)
+	{
+		//TODO: Leia talle s6itja, kes ei ole proovitud s6itjate listis.
+		//TODO: Seadista staatus yheks.
+		//TODO: MÃÃra viimase uuenduse aeg.
+	}
+}
+
+function getClosestDriverNotInList($lat, $lng, $list)
+{
+	$lastDistance = 10000000;
+	$lastDriver = -1;
+	$driverQuery = $mysqli->query("SELECT * FROM " . $mysql['pref'] . "kasutajad WHERE `aktiivne` = '1'");
+	while($row = $driverQuery->fetch_assoc())
+	{
+		if(!in_array($row['sqlid'], $list))
+		{
+			$distance = getDistanceBetweenPoints($lat, $lng, $row['lat'], $row['lng']);
+			if($distance < $lastDistance)
+			{
+				$lastDistance = $distance;
+				$lastDriver = $row['sqlid'];
+			}
+		}
+	}
+	return $lastDriver;
+}
+
+function getDistanceBetweenPoints($lat1, $lng1, $lat2, $lng2)
+{
+	$dlat = deg2rad($lat2 - $lat1);
+	$dlng = deg2rad($lng2 - $lng1);
+	$a = sin($dlat/2) * sin($dlat/2) + sin($dlng/2) * sin($dlng/2) * cos(deg2rad($lat1)) * cos(deg2rad($lat2));
+	$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+	return 6371 * $c;
+}
 
 $mysqli->close();
 
